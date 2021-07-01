24e2b2f2da34aaff4d14955bfd895a1eab5cdd39
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-01 10:45:29 -0600

    strayNum

---
+++ Ruby/level_7/StrayNum.rb
@@ -0,0 +1,9 @@
+# https://www.codewars.com/kata/57f609022f4d534f05000024
+# 7kyu
+# Given an odd-length array of integers, in which all of them are the same, except for one single number.
+# Complete the method which accepts such an array, and returns that single different number.
+# The input array will always be valid! (odd-length >= 3)
+
+def stray (n)
+  n.count(n[0]) == 1 ? n[0] : n.uniq[1]
+end
