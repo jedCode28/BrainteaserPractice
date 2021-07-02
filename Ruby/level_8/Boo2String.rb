59e23c0fb2059355e4e989a02cc5ebc54f257405
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-02 13:23:09 -0600

    boo to str

---
+++ Ruby/level_8/Boo2String.rb
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd
+# 8kyu
+# I implemented a function which convert the given boolean value into its string representation.
+
+def boolean_to_string(b)
+  return b.to_s
+end
