11c3d6fb848aa5f6140d86be4c6ffc5848804cbd
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-09 10:12:58 -0600

    remove string spaces

---
+++ Ruby/level_8/RmSpaces.rb
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57eae20f5500ad98e50002c5
+# 8kyu
+# My function removes the spaces from the string, then returns the resultant string.
+
+def no_space(x)
+  x.delete(' ')
+end
