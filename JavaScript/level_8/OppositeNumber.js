08e32cc9d4ca2dd0ddbe34559ba2fed7f759165c
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-12 13:37:59 -0600

    opposite number

---
+++ JavaScript/level_8/OppositeNumber.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/56dec885c54a926dcd001095
+// 8kyu
+// My function returns the opposite of the number it is given.
+
+function opposite(number) {
+  return (number - number * 2)
+}
