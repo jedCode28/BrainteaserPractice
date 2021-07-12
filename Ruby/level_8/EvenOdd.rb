419c93cb6c129530bf4ddae6ded6d76c1a63a9fc
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-12 13:39:56 -0600

    even or odd ruby

---
+++ Ruby/level_8/EvenOdd.rb
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/53da3dbb4a5168369a0000fe
+# 8kyu
+# I created a function (or write a script in Shell) that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+
+def even_or_odd(number)
+  if number % 2 == 0
+    'Even'
+  else 
+    'Odd'
+  end 
+end
