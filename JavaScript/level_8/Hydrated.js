68a4769a67c67d8d1b90015862fbfdbcaf573491
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-12 13:36:09 -0600

    hydrated

---
+++ JavaScript/level_8/Hydrated.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/582cb0224e56e068d800003c
+// 8kyu
+// My function, given time measured in hours, returns the number of liters of water neededl
+
+function litres(time) {
+  return Math.floor(time * 0.5)
+}
