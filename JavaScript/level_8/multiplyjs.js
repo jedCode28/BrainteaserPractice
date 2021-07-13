d7d22f612584aa78cf3042c98fbfcffd8e2976b9
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-13 10:38:40 -0600

    multiply javascript

---
+++ JavaScript/level_8/multiplyjs.js
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/50654ddff44f800200000004
+// 8kyu
+// Given a broken function, I fixed it to properly return a product of 2 numbers.
+
+function multiply(a, b){
+  return a * b
+}
