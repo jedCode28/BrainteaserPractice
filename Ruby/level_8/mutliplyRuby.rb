d0c5a0a55956d3695517f5c3686b8f6ea7a0500a
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-13 10:40:49 -0600

    multiply ruby

---
+++ Ruby/level_8/mutliplyRuby.rb
@@ -0,0 +1,8 @@
+# https://www.codewars.com/kata/50654ddff44f800200000004
+# 8kyu
+# Given a broken function, I fixed it to properly return a product of 2 numbers.
+
+
+def multiply(a, b)
+  a * b
+end
