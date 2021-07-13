4ff52fdbf3cedc8d0a0002436c7049d5950e7f31
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-13 10:13:36 -0600

    string repeat

---
+++ Ruby/level_8/string_pete.rb
@@ -0,0 +1,7 @@
+# https://www.codewars.com/kata/57a0e5c372292dd76d000d7e
+# 8kyu
+# I wrote a function called repeatStr which repeats the given string string exactly n times.
+
+def repeat_str (n, s)
+  s * n
+end
