401785533b7ff25eda572cde1adcb761b7d2b006
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-30 15:02:28 -0600

    counting unique occurences of each element in the array

---
+++ Ruby/level_7/countArrElms.rb
@@ -0,0 +1,9 @@
+https://www.codewars.com/kata/5569b10074fe4a6715000054
+7kyu
+I wrote a function that takes an array and counts the number of each unique element present.
+
+def count(r)
+  c = Hash.new(0)
+  r.each { |z| c[z] += 1}
+  c
+end
