ae398a80e67c1f7b2b88580107948c20be5f33d4
Jedediah Wood <jedediahwood28@gmail.com>
2021-07-31 11:18:35 -0600

    fix

--- Ruby/level_7/countArrElms.rb
+++ Ruby/level_7/countArrElms.rb
@@ -1,6 +1,6 @@
-https://www.codewars.com/kata/5569b10074fe4a6715000054
-7kyu
-I wrote a function that takes an array and counts the number of each unique element present.
+# https://www.codewars.com/kata/5569b10074fe4a6715000054
+# 7kyu
+# I wrote a function that takes an array and counts the number of each unique element present.
 
 def count(r)
   c = Hash.new(0)
