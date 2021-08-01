f79c789c76358bc02ba98f315dc8223035dd40fb
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-01 15:10:28 -0600

    sql begins with, ends with

--- SQL/zoo.sql
+++ SQL/zoo.sql
@@ -2,8 +2,9 @@
 
 -- These are 15 SQL brainteaser problems, based on a database 'WORLD' with names and information of various countries.
 
--- 1. The % is a wild-card it can match any characters
---     Find the country that start with Y
+-- 1. 
+    -- The % is a wild-card it can match any characters
+    -- Find the country that start with Y
 
 SELECT name FROM world
 WHERE name LIKE 'Y%'
@@ -14,4 +15,34 @@ WHERE name LIKE 'Y%'
 -- Yemen
 
 
+-- 2.
+-- Find the countries that end with y
 
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+-- output: 
+
+-- name
+-- Germany
+-- Hungary
+-- Italy
+-- Norway
+-- Paraguay
+-- Turkey
+-- Uruguay
+-- Vatican City
+
+
+-- 3.
+-- Luxembourg has an x - so does one other country. List them both.
+-- Find the countries that contain the letter x
+
+SELECT name FROM world
+  WHERE name LIKE '%X%'
+
+-- output: 
+
+-- name
+-- Luxembourg
+-- Mexico
