0c93527915c1e28f8519cfc7c38e05db4217a081
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-02 10:32:32 -0600

    finished zoo.sql brainteasers

--- SQL/zoo.sql
+++ SQL/zoo.sql
@@ -247,3 +247,43 @@ WHERE capital LIKE concat('%', name, '%')
 -- Panama City	Panama
 -- San Marino	San Marino
 -- Singapore	Singapore
+
+
+-- 14.
+-- Find the capital and the name where the capital is an extension of name of the country.
+-- You should include Mexico City as it is longer than Mexico. You should not include Luxembourg
+--  as the capital is the same as the country.
+
+SELECT capital, name
+FROM world
+WHERE capital LIKE concat(name, '%') AND name <> capital
+
+-- output: 
+
+-- capital	name
+-- Andorra la Vella	Andorra
+-- Guatemala City	Guatemala
+-- Kuwait City	Kuwait
+-- Mexico City	Mexico
+-- Monaco-Ville	Monaco
+-- Panama City	Panama
+
+
+-- 15.
+-- For Monaco-Ville the name is Monaco and the extension is -Ville.
+-- Show the name and the extension where the capital is an extension of name of the country.
+
+SELECT name, REPLACE(capital, name,'') AS ext
+FROM world
+WHERE capital LIKE concat('%',name,'%') AND capital > name;
+
+-- output: 
+
+
+-- name	ext
+-- Andorra	la Vella
+-- Guatemala	City
+-- Kuwait	City
+-- Mexico	City
+-- Monaco	-Ville
+-- Panama	City
