2c47d73800b06212d97ad50d4bb7efac4f37038b
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-03 11:22:30 -0600

    finished problems

--- SQL/SELECT_basics.sql
+++ SQL/SELECT_basics.sql
@@ -16,3 +16,34 @@ SELECT population FROM world
 -- 80716000
 
 
+-- 2.
+-- Show the name and the population for 'Sweden', 'Norway' and 'Denmark'.
+
+SELECT name, population FROM world
+  WHERE name IN ('Sweden', 'Denmark', 'Norway');
+
+-- output: 
+
+-- name	population
+-- Denmark	5634437
+-- Norway	5124383
+-- Sweden	9675885
+
+
+-- 3.
+-- Show the country and the area for countries with an area between 200,000 and 250,000.
+
+SELECT name, area FROM world
+  WHERE area BETWEEN 200000 AND 250000
+
+-- output: 
+
+-- name	area
+-- Belarus	207600
+-- Ghana	238533
+-- Guinea	245857
+-- Guyana	214969
+-- Laos	236800
+-- Romania	238391
+-- Uganda	241550
+-- United Kingdom	242900
