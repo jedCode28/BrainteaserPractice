144160fda8445f90f978eb5e55865809337f4b2a
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-04 12:23:27 -0600

    init wednesday commit

--- SQL/SELECT_select.sql
+++ SQL/SELECT_select.sql
@@ -181,3 +181,43 @@ WHERE continent = 'Europe'
 -- Vatican City	0%
 
 
+-- 6.
+-- Which countries have a GDP greater than every country in Europe?
+
+SELECT name
+FROM world
+WHERE gdp > (SELECT MAX(gdp)
+FROM world
+WHERE continent = 'Europe')
+
+-- output: 
+
+-- name
+-- China
+-- Japan
+-- United States
+
+
+-- 7.
+-- Find the largest country (by area) in each continent, show the continent, the name and the area:
+
+SELECT continent, name, area 
+FROM world x
+WHERE area >= ALL
+    (SELECT area FROM world y
+        WHERE y.continent = x.continent
+          AND area > 0)
+
+-- output: 
+
+-- continent	name	area
+-- Africa	Algeria	2381741
+-- Oceania	Australia	7692024
+-- South America	Brazil	8515767
+-- North America	Canada	9984670
+-- Asia	China	9596961
+-- Caribbean	Cuba	109884
+-- Europe	Kazakhstan	2724900
+-- Eurasia	Russia	17125242
+
+
