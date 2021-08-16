c19b794cdb41183bc7102de9982753b956c0079d
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-16 12:50:53 -0600

    complete!!!

--- SQL/SELECT_world.sql
+++ SQL/SELECT_world.sql
@@ -297,4 +297,57 @@ WHERE LEN(name) = LEN(capital)
 -- Zambia	Lusaka
 
 
- 
+-- 12.
+-- Show the name and the capital where the first letters of each match. 
+-- Don't include countries where the name and the capital are the same word.
+
+SELECT name, capital
+FROM world
+WHERE LEFT(name,1) = LEFT(capital,1) AND
+      name <> capital
+
+-- output: 
+
+-- name	capital
+-- Algeria	Algiers
+-- Andorra	Andorra la Vella
+-- Barbados	Bridgetown
+-- Belize	Belmopan
+-- Brazil	Brasília
+-- Brunei	Bandar Seri Begawan
+-- Burundi	Bujumbura
+-- Guatemala	Guatemala City
+-- Guyana	Georgetown
+-- Kuwait	Kuwait City
+-- Maldives	Malé
+-- Marshall Islands	Majuro
+-- Mexico	Mexico City
+-- Monaco	Monaco-Ville
+-- Mozambique	Maputo
+-- Niger	Niamey
+-- Panama	Panama City
+-- Papua New Guinea	Port Moresby
+-- Sao Tomé and Príncipe	São Tomé
+-- South Korea	Seoul
+-- Sri Lanka	Sri Jayawardenepura Kotte
+-- Sweden	Stockholm
+-- Taiwan	Taipei
+-- Tunisia	Tunis
+
+
+-- 13.
+-- Find the country that has all the vowels and no spaces in its name.
+
+SELECT name
+FROM world
+WHERE name LIKE '%a%' AND
+      name LIKE '%e%' AND 
+      name LIKE '%i%' AND 
+      name LIKE '%o%' AND
+      name LIKE '%u%' AND 
+      name NOT LIKE '% %'
+
+-- output: 
+
+-- name
+-- Mozambique
