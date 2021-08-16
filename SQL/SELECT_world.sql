d0bff0689d4dfdef284cc88e3d0138089e5e3727
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-16 10:46:30 -0600

    init

---
+++ SQL/SELECT_world.sql
@@ -0,0 +1,7 @@
+-- https://sqlzoo.net/wiki/SELECT_from_WORLD_Tutorial
+
+-- These are 13 SQL excercises that query from a 'World' database, focusing on the SELECT method.
+
+-- 1.
+-- Observe the result of running this SQL command to show the name, continent and population of all countries.
+
