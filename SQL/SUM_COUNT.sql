147a3e776fb251773eca1ccaa5756d32da7d9773
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-17 11:50:04 -0600

    all finished!

--- SQL/SUM_COUNT.sql
+++ SQL/SUM_COUNT.sql
@@ -56,3 +56,76 @@ WHERE area > 1000000
 -- 28
 
 
+-- 5.
+-- What is the total population of ('Estonia', 'Latvia', 'Lithuania')
+
+SELECT SUM(population)
+FROM world
+WHERE name = 'Estonia' OR
+      name = 'Latvia' OR
+      name = 'Lithuania'
+
+-- output: 
+
+-- 6251750
+
+
+-- 6.
+-- For each continent show the continent and number of countries.
+
+SELECT continent, COUNT(name)
+FROM world
+GROUP BY continent
+
+-- output: 
+
+-- continent	
+-- Africa	53
+-- Asia	47
+-- Caribbean	11
+-- Eurasia	2
+-- Europe	44
+-- North America	11
+-- Oceania	14
+-- South America	13
+
+
+-- 7.
+-- For each continent show the continent and number of countries with populations of 
+-- at least 10 million.
+
+SELECT continent, COUNT(name)
+FROM world
+WHERE population > 10000000
+GROUP BY continent
+
+-- output: 
+
+-- continent	
+-- Africa	29
+-- Asia	26
+-- Caribbean	2
+-- Eurasia	1
+-- Europe	14
+-- North America	4
+-- Oceania	1
+-- South America	8
+
+
+-- 8.
+-- List the continents that have a total population of at least 100 million.
+
+SELECT DISTINCT(continent)
+FROM world
+GROUP BY continent
+HAVING SUM(population) > 100000000
+
+-- output: 
+
+-- continent
+-- Africa
+-- Asia
+-- Eurasia
+-- Europe
+-- North America
+-- South America
