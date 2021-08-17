6bd632fb61456372a569837aa3175becb7717a33
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-17 11:28:44 -0600

    init sum count

---
+++ SQL/SUM_COUNT.sql
@@ -0,0 +1,15 @@
+-- https://sqlzoo.net/wiki/SUM_and_COUNT
+
+-- These are 8 SQL excercises focusing on aggregate functions. 
+
+-- 1.
+-- Show the total population of the world.
+
+SELECT SUM(population)
+FROM world
+
+-- output: 
+
+-- 7118632738
+
+
