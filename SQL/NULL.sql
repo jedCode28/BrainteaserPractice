c8c9b8293255799194b9d264f12561be6691be31
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-18 12:56:42 -0600

    finished with NULL practice problems

--- SQL/NULL.sql
+++ SQL/NULL.sql
@@ -84,3 +84,88 @@ FROM teacher
 -- Deadyawn	07986 444 2266
 
 
+-- 6.
+-- Use the COALESCE function and a LEFT JOIN to print the teacher name and department name. 
+-- Use the string 'None' where there is no department.
+
+SELECT teacher.name, COALESCE(dept.name, 'None')
+FROM teacher LEFT JOIN dept ON teacher.dept=dept.id
+
+-- output: 
+
+-- name	
+-- Shrivell	Computing
+-- Throd	Computing
+-- Splint	Computing
+-- Spiregrain	None
+-- Cutflower	Design
+-- Deadyawn	None
+
+
+-- 7.
+-- Use COUNT to show the number of teachers and the number of mobile phones.
+
+SELECT COUNT(name), COUNT(mobile)
+FROM teacher
+
+-- output: 
+
+-- 6	3
+
+
+-- 8.
+-- Use COUNT and GROUP BY dept.name to show each department and the number of staff.
+-- Use a RIGHT JOIN to ensure that the Engineering department is listed.
+
+SELECT dept.name, COUNT(teacher.name)
+FROM teacher RIGHT JOIN dept ON teacher.dept=dept.id
+GROUP BY dept.name
+
+-- output: 
+
+-- name	
+-- Computing	3
+-- Design	1
+-- Engineering	0
+
+
+-- 9.
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher is in dept 1 or 2 and 'Art' otherwise.
+
+SELECT name, CASE WHEN teacher.dept=1 THEN 'Sci'
+                  WHEN teacher.dept=2 THEN 'Sci'
+                  ELSE 'Art'
+             END 
+FROM teacher
+
+-- output: 
+
+-- name	
+-- Shrivell	Sci
+-- Throd	Sci
+-- Splint	Sci
+-- Spiregrain	Art
+-- Cutflower	Sci
+-- Deadyawn	Art
+
+
+-- 10.
+-- Use CASE to show the name of each teacher followed by 'Sci' if the teacher 
+-- is in dept 1 or 2, show 'Art' if the teacher's dept is 3 and 'None' otherwise.
+
+SELECT name, CASE WHEN teacher.dept=1 THEN 'Sci'
+                  WHEN teacher.dept=2 THEN 'Sci'
+                  WHEN teacher.dept=3 THEN 'Art'
+                  ELSE 'None'
+             END 
+FROM teacher
+
+-- output: 
+
+-- name	
+-- Shrivell	Sci
+-- Throd	Sci
+-- Splint	Sci
+-- Spiregrain	None
+-- Cutflower	Sci
+-- Deadyawn	None
