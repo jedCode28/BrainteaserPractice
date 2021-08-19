25e54fc9ebfefeb4284b9134e518622a3ff3efb2
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-19 12:11:36 -0600

    Halfway down

--- SQL/SELF_JOIN.sql
+++ SQL/SELF_JOIN.sql
@@ -47,3 +47,33 @@ WHERE company = 'LRT' AND num = '4'
 -- 117	Hillend
 
 
+-- 4.
+-- Add a HAVING clause to restrict the output to these two routes.
+
+SELECT company, num, COUNT(*)
+FROM route WHERE stop=149 OR stop=53
+GROUP BY company, num
+HAVING COUNT(*) = 2
+
+-- output: 
+
+-- company	num	
+-- LRT	4	2
+-- LRT	45	2
+
+
+-- 5.
+-- Change the query so that it shows the services from Craiglockhart to London Road.
+
+SELECT a.company, a.num, a.stop, b.stop
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=53 AND b.stop=149
+
+-- output: 
+
+-- company	num	stop	stop
+-- LRT	4	53	149
+-- LRT	45	53	149
+
+
