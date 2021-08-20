303ed66e7187053f3fe0735cfa3ac70404f8b0b9
Jedediah Wood <jedediahwood28@gmail.com>
2021-08-20 13:16:27 -0600

    I AM COMPLEEEEEETE

--- SQL/SELF_JOIN.sql
+++ SQL/SELF_JOIN.sql
@@ -77,3 +77,186 @@ WHERE a.stop=53 AND b.stop=149
 -- LRT	45	53	149
 
 
+-- 6.
+-- Change the query so that the services between 'Craiglockhart' and 'London Road' are shown.
+
+SELECT a.company, a.num, stopa.name, stopb.name
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND 
+      stopb.name='London Road'
+
+-- output: 
+
+-- company	num	name	name
+-- LRT	4	Craiglockhart	London Road
+-- LRT	45	Craiglockhart	London Road
+
+
+-- 7.
+-- Give a list of all the services which connect stops 115 and 137 ('Haymarket' and 'Leith')
+
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON a.num=b.num
+WHERE a.stop='115' AND b.stop='137'
+
+-- output: 
+
+-- company	num
+-- LRT	12
+-- LRT	2
+-- LRT	22
+-- LRT	25
+-- LRT	2A
+-- SMT	C5
+
+
+-- 8.
+-- Give a list of the services which connect the stops 'Craiglockhart' and 'Tollcross'
+
+SELECT a.company, a.num
+FROM route a JOIN route b ON a.num=b.num
+JOIN stops stopa ON (a.stop=stopa.id)
+JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart' AND
+      stopb.name='Tollcross'
+
+-- output: 
+
+-- company	num
+-- LRT	10
+-- LRT	27
+-- LRT	45
+-- LRT	47
+
+
+-- 9.
+-- Give a distinct list of the stops which may be reached from 'Craiglockhart' by taking one bus, including 'Craiglockhart' itself, offered by the LRT company. 
+-- Include the company and bus no. of the relevant services.
+
+SELECT DISTINCT bstop.name, a.company, a.num
+FROM route AS a JOIN route AS b ON a.company=b.company AND
+                          a.num=b.num
+                JOIN stops AS astop ON a.stop=astop.id
+                JOIN stops AS bstop ON b.stop=bstop.id
+WHERE astop.name='Craiglockhart'
+
+-- output: 
+
+-- name	company	num
+-- Balerno	LRT	47
+-- Balerno Church	LRT	47
+-- Bingham	LRT	4
+-- Brunstane	LRT	45
+-- Canonmills	LRT	27
+-- Canonmills	LRT	47
+-- Cockburn Crescent	LRT	47
+-- Colinton	LRT	10
+-- Colinton	LRT	45
+-- Colinton	LRT	47
+-- Craiglockhart	LRT	10
+-- Craiglockhart	LRT	27
+-- Craiglockhart	LRT	4
+-- Craiglockhart	LRT	45
+-- Craiglockhart	LRT	47
+-- Crewe Toll	LRT	27
+-- Currie	LRT	45
+-- Currie	LRT	47
+-- Duddingston	LRT	45
+-- Fairmilehead	LRT	4
+-- Hanover Street	LRT	27
+-- Hanover Street	LRT	45
+-- Hanover Street	LRT	47
+-- Haymarket	LRT	4
+-- Hillend	LRT	4
+-- Hunters Tryst	LRT	27
+-- Leith	LRT	10
+-- Leith Walk	LRT	10
+-- London Road	LRT	4
+-- London Road	LRT	45
+-- Muirhouse	LRT	10
+-- Newhaven	LRT	10
+-- Northfield	LRT	4
+-- Northfield	LRT	45
+-- Oxgangs	LRT	27
+-- Oxgangs	LRT	4
+-- Princes Street	LRT	10
+-- Princes Street	LRT	4
+-- Riccarton Campus	LRT	45
+-- Silverknowes	LRT	10
+-- Silverknowes	LRT	27
+-- Tollcross	LRT	10
+-- Tollcross	LRT	27
+-- Tollcross	LRT	45
+-- Tollcross	LRT	47
+-- Torphin	LRT	10
+
+
+-- 10.
+-- Find the routes involving two buses that can go from Craiglockhart to Lochend.
+-- Show the bus no. and company for the first bus, the name of the stop for the transfer,
+-- and the bus no. and company for the second bus.
+
+SELECT DISTINCT one.num as FirstBus, one.company as FirstComp, one.name as Transfer, two.num as SecBus, two.company as SecComp
+
+FROM
+
+    (select distinct a.num, a.company, yy.name
+     from route a join route b on (a.company=b.company and a.num=b.num) 
+                  join stops xx on (xx.id=a.stop) 
+                  join stops yy on (yy.id=b.stop)
+     where xx.name='Craiglockhart' and yy.name<>'Lochend'
+     ) AS one
+
+JOIN
+
+    (select distinct c.num, d.company, mm.name
+     from route c join route d on (c.company=d.company and c.num=d.num) 
+                  join stops mm on (mm.id=c.stop) 
+                  join stops nn on (nn.id=d.stop)
+     where mm.name <> 'Craiglockhart' and nn.name='Lochend'
+     ) AS two
+
+ON (two.name=one.name)
+
+-- output: 
+
+-- FirstBus	FirstComp	Transfer	SecBus	SecComp
+-- 10	LRT	Leith	34	LRT
+-- 10	LRT	Leith	35	LRT
+-- 10	LRT	Leith	87	LRT
+-- 10	LRT	Leith	C5	SMT
+-- 10	LRT	Princes Street	65	LRT
+-- 10	LRT	Princes Street	C5	SMT
+-- 27	LRT	Canonmills	34	LRT
+-- 27	LRT	Canonmills	35	LRT
+-- 27	LRT	Crewe Toll	20	LRT
+-- 4	LRT	Haymarket	65	LRT
+-- 4	LRT	Haymarket	C5	SMT
+-- 4	LRT	London Road	20	LRT
+-- 4	LRT	London Road	34	LRT
+-- 4	LRT	London Road	35	LRT
+-- 4	LRT	London Road	42	LRT
+-- 4	LRT	London Road	46A	LRT
+-- 4	LRT	London Road	65	LRT
+-- 4	LRT	London Road	87	LRT
+-- 4	LRT	London Road	87A	LRT
+-- 4	LRT	London Road	C5	SMT
+-- 4	LRT	Princes Street	65	LRT
+-- 4	LRT	Princes Street	C5	SMT
+-- 45	LRT	Duddingston	42	LRT
+-- 45	LRT	Duddingston	46A	LRT
+-- 45	LRT	London Road	20	LRT
+-- 45	LRT	London Road	34	LRT
+-- 45	LRT	London Road	35	LRT
+-- 45	LRT	London Road	42	LRT
+-- 45	LRT	London Road	46A	LRT
+-- 45	LRT	London Road	65	LRT
+-- 45	LRT	London Road	87	LRT
+-- 45	LRT	London Road	87A	LRT
+-- 45	LRT	London Road	C5	SMT
+-- 45	LRT	Riccarton Campus	65	LRT
+-- 47	LRT	Canonmills	34	LRT
+-- 47	LRT	Canonmills	35	LRT
